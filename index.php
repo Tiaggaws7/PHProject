bb87ff6a078ba4be842e7337e8122e79b73034ab
Tiaggaws7 <65310974+Tiaggaws7@users.noreply.github.com>
2021-11-16 13:50:21 +0100

    Update and rename index.html to index.php
    changement de index.html et index.php
    ajout du lien avec style.css
    rajout d'un template pour afficher les CD
    la connexion à base de données ne marche pas donc je l'ai mis en commentaire

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+
+    <p>voici un paragraphe</p>
+
+
+    <div class="cd_box">
+
+        <div class="caracteristique" >        
+            <p class="titre">Jeannine</p>
+            <p class="auteur">Lomepal</p>
+            <p class="prix" >19,99€</p>
+        </div>
+        <img class="pochette" src="Lomepal.jpg"/>
+    </div>
+<!-- 
+    début commentaire
+    <?php 
+
+        $hostname = "lakartxela.iutbayonne.univ-pau.fr";
+        $user = "tbuil";
+        $password = "tbuil";
+        $bdd = "tbuil";
+
+        mysqli_connect($hostname, $user, $password, $bdd);
+        session_start();
+
+         
+    
+    ?>
+    fin du commentaire
+-->
+
+</body>
+</html>
