39efd512a197a7b4de4e35200e55528b1092fdee
Tiaggaws7 <65310974+Tiaggaws7@users.noreply.github.com>
2021-11-17 16:58:05 +0100

    Update bdd_xml_cd.php

--- bdd_xml_cd.php
+++ bdd_xml_cd.php
@@ -2,21 +2,21 @@
 $xmlstr = <<<XML
 <?xml version="1.0" encoding="ISO-8859-1"?>
 <bdd>
-    <CD id=1>
+    <CD>
         <titre>30</titre>
         <auteur>Adele</auteur>
         <genre>pop</genre>
         <prix>15</prix>
         <image>https://static.fnac-static.com/multimedia/Images/FR/NR/5d/a8/bf/12560477/1507-1/tsp20211018195140/30-Edition-Limitee-Exclusivite-Fnac-Double-Vinyle-transparent.jpg</image>
     </CD>
-    <CD id=2>
+    <CD>
         <titre>Vivante</titre>
         <auteur>Amel Bent</auteur>
         <genre>pop</genre>
         <prix>13</prix>
         <image>https://m.media-amazon.com/images/I/81r8pSIbu-L._SY355_.jpg</image>
     </CD>
-    <CD id=3> 
+    <CD> 
         <titre></titre>
         <auteur></auteur>
         <genre></genre>
