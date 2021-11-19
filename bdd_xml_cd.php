4d686baf9a17262d21c84570cd29d2e966759c0e
Tiaggaws7 <65310974+Tiaggaws7@users.noreply.github.com>
2021-11-19 18:57:13 +0100

    Update bdd_xml_cd.php
    ajout de la balise id

--- bdd_xml_cd.php
+++ bdd_xml_cd.php
@@ -2,21 +2,24 @@
 $xmlstr = <<<XML
 <?xml version="1.0" encoding="ISO-8859-1"?>
 <bdd>
-    <CD>
+    <CD >
+    <id>1</id>
         <titre>30</titre>
         <auteur>Adele</auteur>
         <genre>pop</genre>
-        <prix>15</prix>
-        <image>https://static.fnac-static.com/multimedia/Images/FR/NR/5d/a8/bf/12560477/1507-1/tsp20211018195140/30-Edition-Limitee-Exclusivite-Fnac-Double-Vinyle-transparent.jpg</image>
+        <prix>20</prix>
+        <image>Pochette_Adele_30.jpg</image>
     </CD>
-    <CD>
+    <CD >
+        <id>2</id>
         <titre>Vivante</titre>
         <auteur>Amel Bent</auteur>
         <genre>pop</genre>
-        <prix>13</prix>
-        <image>https://m.media-amazon.com/images/I/81r8pSIbu-L._SY355_.jpg</image>
+        <prix>14,99</prix>
+        <image>Pochette_Amel_Bent_Vivante.jpg</image>
     </CD>
-    <CD> 
+    <CD > 
+        <id>3</id>
         <titre></titre>
         <auteur></auteur>
         <genre></genre>
