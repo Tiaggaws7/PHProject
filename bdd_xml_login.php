669b56224c631c59157ebb9b8cc1a88e51f4fd60
LucasDarraillan <93929404+LucasDarraillan@users.noreply.github.com>
2021-11-21 15:51:36 +0100

    Add files via upload

---
+++ bdd_xml_login.php
@@ -0,0 +1,16 @@
+<?php 
+$xmllogin = <<<XML
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<bdd>
+    <USER > 
+        <login>root</login>
+        <mdp>root</mdp>
+    </USER>
+    <USER > 
+        <login>lucas</login>
+        <mdp>darraillan</mdp>
+    </USER>
+
+</bdd>
+XML;
+?>
