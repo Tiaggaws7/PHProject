c71eab2736b072289bb94a3602aa9400da625155
LucasDarraillan <93929404+LucasDarraillan@users.noreply.github.com>
2021-11-22 22:51:33 +0100

    Add files via upload

---
+++ ajouter_cd.php
@@ -0,0 +1,67 @@
+<?php
+
+    // Fonction premettant d'inserer du texte a une ligne donnée
+    function finsert_ligne($fichier , $texte , $i){
+
+        // on lit le fichier dans un tableau.
+            $contenu_fichier = file($fichier);
+        // on ajoute le texte a la fin de la $i-ieme ligne.
+            $contenu_fichier[$i]=trim($texte)."\n".trim($contenu_fichier[$i])." \n";
+        // on va ecrire le nouveau contenu dans le fichier
+            $fp = fopen ($fichier, "w+");
+        //on re-insere toutes les lignes du tableau dans le fichier
+            for ($k=0;$k<sizeof($contenu_fichier);$k++) {
+                fwrite($fp, (trim($contenu_fichier[$k])." \n"));
+            };
+            fclose ($fp);
+            return true;
+    }
+    
+    // Base de données
+    include 'copie_bdd_xml_cd.php';
+    $bdd = 'copie_bdd_xml_cd.php';
+
+    // Upload de l'image
+    require 'uploadFile.php';
+    $upload = new uploadFile();
+    if(isset($_POST['Enregistrer']) && !empty($_POST['Enregistrer']))
+    {
+            $tmp_name=$_FILES['image']['tmp_name'];
+            $name=$_FILES['image']['name'];
+            $upload->upload($tmp_name, $name);
+    }
+
+    // Ligne de l'entrer dans la base de données d'un CD -1
+    $line = 4;
+    
+    // Génération d'un identifiant unique
+    $newId = uniqid();
+     
+    // Récuperration des valeurs à rentrer dans la base de données
+    $titreCD = $_POST['titre'];
+    $auteurCD = $_POST['auteur'];
+    $genreCD = $_POST['genre'];
+    $prixCD = $_POST['prix'];
+    if(!empty ($name)){
+        $imageCD = date("G-i-s").$name;
+    }else { $imageCD = "icon_image.png"; }
+    
+
+    // Insertion des valeurs du CD dans la base de données
+    finsert_ligne($bdd, "<CD>", $line); $line++;
+    finsert_ligne($bdd, "<id>$newId</id>", $line); $line++;
+    finsert_ligne($bdd, "<titre>$titreCD</titre>", $line); $line++;
+    finsert_ligne($bdd, "<auteur>$auteurCD</auteur> ", $line); $line++;
+    finsert_ligne($bdd, "<genre>$genreCD</genre>", $line); $line++;
+    finsert_ligne($bdd, "<prix>$prixCD</prix> ", $line); $line++;
+    finsert_ligne($bdd, "<image>$imageCD</image> ", $line); $line++;
+    finsert_ligne($bdd, "</CD>", $line); $line++;
+
+    // On remplace la ligne : $xmlstr = <<<XML , car un espace se génère et cela entraine un disfonctionnement de la base de donées
+    file_put_contents($bdd, str_replace('$xmlstr = <<<XML ', '$xmlstr = <<<XML', file_get_contents($bdd)));
+    
+    header("Location: nomPageAdmin.php");
+    die();
+?>
+
+    
---
+++ connexion.php
@@ -0,0 +1,64 @@
+<?php 
+    // Démarrage de la session
+    session_start(); 
+    // On inclut la connexion à la base de données grace au fichier config.php
+    require_once 'config.php'; 
+
+    // Si les champs login et password ne sont pas vident
+    if(!empty($_POST['login']) && !empty($_POST['password'])) 
+    {
+        // Encodage
+        $login = htmlspecialchars($_POST['login']); 
+        $password = htmlspecialchars($_POST['password']);
+        
+        // Identifiant transformé en minuscule
+        $login = strtolower($login); 
+
+        // Initialisation des variables
+        $nb_login = $bdd->count();
+        $identifant = "notOk";
+        $motDePasse = "notOk";
+
+        //Vérification de l'identifiant et du mot de passe
+        for($i = 0; $i <= $nb_login - 1; $i++ ){
+            if($bdd->USER[$i]->login == $login){
+                $identifant = "ok";
+                if($bdd->USER[$i]->mdp == $password){
+                    $motDePasse = "ok";
+                }
+            }
+            if(($bdd->USER[$i]->login == $login) && ($login == "root")){
+                $identifant = "root";
+                if(($bdd->USER[$i]->mdp == $password) && ($password == "root")){
+                    $motDePasse = "root";
+                }
+            }
+        }
+
+        // Action en fonction de la validité de l'identifiant et du mot de passe
+        if($identifant == "ok"){ 
+            if($motDePasse == "ok"){ 
+                header('Location: home.php');
+                die();
+            }
+            else{ 
+                header('Location: index.php?login_err=password'); 
+                die(); 
+            }
+        }
+        elseif($identifant == "root"){
+            if($motDePasse == "root"){ 
+                header('Location: nomPageAdmin.php');
+                die();
+            }
+            else{ 
+                header('Location: index.php?login_err=password'); 
+                die(); 
+            }
+        }
+        else{ 
+            header('Location: index.php?login_err=email'); 
+            die(); 
+        }
+    }
+?>
