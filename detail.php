19e9f0008e443fb21c21fc9bd6f8fcdea719339a
Tiaggaws7 <65310974+Tiaggaws7@users.noreply.github.com>
2021-11-23 10:34:40 +0100

    Update detail.php

--- detail.php
+++ detail.php
@@ -4,9 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="styles_article.css" rel="stylesheet">
+    <link href="styles_detail.css" rel="stylesheet">
     <title>SuperCD article</title>
-</head>
+</head> 
+
+<nav class="header_nav">
+    <a href="index.php">Accueil</a>
+    <p>SuperCD</p>
+    <a href="panier.php"><img alt="logo panier" src="panier.png"/></a>
+</nav>
+
 <body>
 
     <?php
@@ -26,10 +33,10 @@
     ?>
                 <div class="cd_box">
                     <div class="caracteristique" >
-                        <p class="titre">Titre : <strong><?=$bdd->CD[$i]->titre?></strong></p>
+                        <p class="titre">Titre : <strong><?=$bdd->CD[$i]->titre?></strong></p> 
                         <p class="auteur">Auteur : <strong><?=$bdd->CD[$i]->auteur?></strong></p>
                         <p class="genre" >Genre :<strong> <?=$bdd->CD[$i]->genre?></strong></p>
-                        <p class="prix" >Prix :<strong> <?=$bdd->CD[$i]->prix?>€</strong></p>
+                        <p class="prix" >Prix :<strong> <?=$bdd->CD[$i]->prix?> €</strong></p>
                     </div>
                     <img class="pochette" src="<?=$bdd->CD[$i]->image?>"/>
                 </div>
