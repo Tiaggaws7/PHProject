5be37dcd7156d738dee092efc4e2102286eba806
LucasDarraillan <93929404+LucasDarraillan@users.noreply.github.com>
2021-11-23 14:50:10 +0100

    Add files via upload

---
+++ supprimer_cd.php
@@ -0,0 +1,60 @@
+<?php
+    $bdd = "bdd_xml_cd.php";
+
+    function suppr_line($file, $line)
+    {
+        $ptr = fopen($file, "r");
+        $contenu = fread($ptr, filesize($file));
+            
+        fclose($ptr);
+        
+        $contenu = explode(PHP_EOL, $contenu); 
+
+        unset($contenu[$line]); 
+        $contenu = array_values($contenu); 
+            
+        $contenu = implode(PHP_EOL, $contenu);
+        $ptr = fopen($file, "w");
+        fwrite($ptr, $contenu);
+        fclose($ptr);
+
+    }
+
+    function suppr_CD($file, $idCD)
+    {
+        // Récupérer le nom des images
+        include $file;
+        
+        $bdd = new SimpleXMLElement($xmlstr);
+        $nb_cd = $bdd->count();
+
+        for($i = 0; $i <= $nb_cd - 1; $i++ ){
+            if($idCD == $bdd->CD[$i]->id){
+                $numCD = $i;
+                $nameImage = $bdd->CD[$i]->image;
+                $nameImageReduite = $bdd->CD[$i]->imageReduite;
+            }    
+        }
+
+        // Calculer le numéro de la première ligne a supprimer -1
+        $line = 4 + 9 * $numCD;
+        // Supprime un CD dans la base de données
+        for ($i=0; $i < 9; $i++) {
+            suppr_line($file, $line);
+        }
+
+        // Supprime les images corrspondantes
+        require 'uploadFile.php';
+        $upload = new uploadFile();
+        if($nameImage != "icon_image.png"){
+            $upload->suppr($nameImage);
+        }
+        if($nameImageReduite != "icon_image.png"){
+            $upload->suppr($nameImageReduite);
+        }
+    }
+    $id = $_GET["id"];
+
+    suppr_CD($bdd,$id);
+    header('Location: back-office.php'); 
+?>
